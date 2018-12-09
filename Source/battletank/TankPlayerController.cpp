3fc1c4465270920f836be1cff0a366a78891ed16
Preet <preet.basson@gmail.com>
2018-12-09 20:15:40 +0000

    V0.0018: Update

--- Source/battletank/TankPlayerController.cpp
+++ Source/battletank/TankPlayerController.cpp
@@ -3,9 +3,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControllerTank();
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Found"));
+    }
+    else {UE_LOG(LogTemp, Warning, TEXT("PlayerController: %s"), *(ControlledTank->GetName()));}
+
+}
 
 ATank* ATankPlayerController::GetControllerTank() const
 {
     return Cast<ATank>(GetPawn());
-};
+}
+
+
 
