a18a7e76cb393a89f51773e4da86d3c4cf75fd15
Preetpal Basson <preet.basson@gmail.com>
2018-12-11 16:37:31 +0000

    V0.0025: Update

--- Source/battletank/Public/Tank.h
+++ Source/battletank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -17,6 +18,7 @@ public:
 	void AimAt(FVector HitLocation);
 
 protected:
+	UTankAimingComponent* TankAimingComponent = nullptr; 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
